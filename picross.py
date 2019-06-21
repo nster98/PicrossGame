a995033e3678222bda908df38db0ae03bbd00c93
Nathan Glikman <nster98@gmail.com>
2019-06-21 00:20:57 -0400

    Added makeArrayFromBoard function

--- picross.py
+++ picross.py
@@ -4,3 +4,39 @@
 # Picross.py
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+from PIL import Image
+import numpy as np
+
+
+def makeArrayFromBoard(image):
+
+    width, height = image.size
+    pixel_values = list(image.getdata())
+    
+    if image.mode == 'RGBA':
+        channels = 4
+    elif image.mode == 'RGB':
+        channels = 3
+    elif image.mode == 'L':
+        channels = 1
+    else:
+        print("Unknown mode: %s" % image.mode)
+        return None
+
+    pixel_values = np.array(pixel_values).reshape((width, height, channels))
+    
+    print("We got here  boys")
+
+    return pixel_values
+
+def main():
+    
+    image = Image.open('board.png', 'r')
+
+    makeArrayFromBoard(image)
+
+if __name__== '__main__': main()
+
+
+
+
